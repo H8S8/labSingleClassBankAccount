895b8e413cf44fb0ade464fc07b948a8ede49ab5
Hannah Riley <hannahriley@Hannahs-MacBook-Air.local>
2024-02-07 14:14:05 +0000

    add BankAccount properties

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,5 +2,10 @@ public class BankAccount {
 
     private String firstName;
     private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
     
+
 }
