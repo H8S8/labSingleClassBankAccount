8c1e9f4183e2df6c89b939e22f8d184ea9c5dad7
Hannah Riley <hannah.847@btinternet.com>
2024-02-07 14:23:15 +0000

    generate getters and setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -8,7 +8,44 @@ public class BankAccount {
 
     //Constructor
     public BankAccount(){
-        
+
     }
 
+    //Getters and Setters
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
